d516906921fb2504af368576907c2643979eff2a
Anil Enjapuri <enjapoori.anil@gmail.com>
2023-01-08 22:40:05 +0530

    first commit

---
+++ src/main/java/com/intel/pexpo/PexpoApplication.java
@@ -0,0 +1,13 @@
+package com.intel.pexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PexpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PexpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intel/pexpo/PexpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intel.pexpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PexpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
